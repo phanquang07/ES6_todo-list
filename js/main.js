bccedd33a9351f3f3f82b47c5c62aab26bbfcd5f
Phan Quang <quangphan0724@gmail.com>
2022-09-30 23:56:20 +0700

    update complete task

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let taskList = []
-// let id = 0
+let taskCompleteList = []
+let serial = 1
 let getELE = (id) => document.querySelector(id)
 
 // Save to local storage
@@ -18,13 +19,13 @@ getLocalStorage()
 // Task List
 let displayTaskList = () => {
   let content = ''
-  taskList.map((task, index) => {
+  taskList.map((task) => {
     content += `
-    <li>${task.content}
-    <div class="task-action">
-      <i class="far fa-trash-alt" onclick="deleteTask('${index}')"></i>
-      <i class="far fa-check-circle"></i>
-    </div>
+    <li id="task-item">${task.content}
+      <div class="task-action">
+        <i class="far fa-trash-alt" onclick="removeTask('${task.id}')"></i>
+        <i id="icon-check" class="far fa-check-circle" onclick="checkStatus('${task.id}')"></i>
+      </div>
     </li>
   `
   })
@@ -38,19 +39,68 @@ let addTask = () => {
   if (taskInput == '') {
     alert('Bạn cần nhập nội dung công việc')
   } else {
-    taskList = [{ content: taskInput, isDone: false }, ...taskList]
+    taskList = [{ id: `t-${serial++}`, content: taskInput, isComplete: false }, ...taskList]
     displayTaskList(taskList.content)
     setLocalStorage()
     getELE('#newTask').value = ''
   }
+  console.log('taskList', taskList);
+  console.log('taskCompleteList', taskCompleteList);
 }
 getELE("#addItem").onclick = addTask
 
-let deleteTask = (index) => {
-  taskList.filter((index) => {
-    return index != index
+// Remove Task
+let removeTask = (id) => {
+  let updateTask = taskList.filter((task) => {
+    return task.id !== id
   })
-  taskList.splice(index, 1)
+  taskList = updateTask
   setLocalStorage()
   displayTaskList()
+  console.log('taskList', taskList);
+}
+
+// Display task complete
+let displayTaskComplete = () => {
+  let content = ''
+  taskCompleteList.map((taskCL) => {
+    content += `
+    <li id="task-item2">${taskCL.content}
+      <div class="task-action">
+        <i class="far fa-trash-alt" onclick="removeTask('${taskCL.id}')"></i>
+        <i id="icon-check" class="far fa-check-circle" onclick="checkStatus('${taskCL.id}')"></i>
+      </div>
+    </li>
+    `
+  })
+  getELE('#completed').innerHTML = content
+}
+displayTaskComplete()
+// Check status
+let checkStatus = (id) => {
+  // console.log(index);
+  // let updateTaskComplete = taskList.filter((task) => {
+  //   return task.id !== id
+  // })
+
+  // taskList[index].isComplete != isComplete
+  // if (taskList[index].isComplete != true) {
+  //   getELE('#icon-check').style.background = 'green'
+  // }
+  // console.log(taskList[index].isComplete);
+
+  // taskCompleteList = updateTaskComplete
+  // console.log('taskList', taskList);
+
+
+  taskList.map((task) => {
+    if (task.id === id) {
+      taskCompleteList.push(task) 
+    }
+  })
+  displayTaskComplete(taskCompleteList)
+  removeTask(id)
+  // taskCompleteList[indexCheck].isComplete = true
+  console.log('taskList', taskList);
+  console.log('taskCompleteList', taskCompleteList);
 }
